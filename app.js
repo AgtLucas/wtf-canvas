dc49ea118d39c6637c5d084a69104332e4a56b07
Lucas Churchill <lucas@agtlucas.com>
2014-07-20 12:08:44 -0300

    Identation...

--- app.js
+++ app.js
@@ -27,11 +27,11 @@ var app = (function (window, document, undefined) {
     var canvas = this.getCanvas('wtf'),
       canvasContext = canvas.getContext('2d');
 
-      canvasContext.fillStyle = "rgb(200,0,0)";
-      canvasContext.fillRect(10, 10, 55, 50);
+    canvasContext.fillStyle = "rgb(200,0,0)";
+    canvasContext.fillRect(10, 10, 55, 50);
 
-      canvasContext.fillStyle = "rgba(0, 0, 200, 0.5)";
-      canvasContext.fillRect(30, 30, 55, 50);
+    canvasContext.fillStyle = "rgba(0, 0, 200, 0.5)";
+    canvasContext.fillRect(30, 30, 55, 50);
   };
 
   // Returnnnnn the objectttttttt!
