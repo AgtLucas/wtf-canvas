a64b1525472fa2243cff38f2cde9a74f941b9d88
Lucas Churchill <lucas@agtlucas.com>
2014-07-20 11:58:47 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+var app = (function (window, document, undefined) {
+
+  'use strict';
+
+  var app = {};
+
+  app.init = function () {
+
+  };
+
+  app.getCanvas = function (id) {
+    var canvas = documen.getElementById(id);
+  };
+
+  return app.init();
+
+})(window, document);
